bcf9918f9f72d18a113191697f7090da76ac2945
Sarit Novik <sanovik@microsoft.com>
2023-06-01 22:04:22 +0300

    fixing ports everywhere

--- docker_app/app_5004.py
+++ docker_app/app_5004.py
@@ -10,4 +10,4 @@ def hello():
     logging.info("after I say hello I say goodbye")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5004)
